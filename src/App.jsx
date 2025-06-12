21b0b89ddc38daec667ae6fc1ce21d11ef35eed4
kushallg <kgadamse@asu.edu>
2025-06-11 17:34:20 -0700

    change imports to resume

--- src/App.jsx
+++ src/App.jsx
@@ -3,59 +3,67 @@ import CyclingScrambleText from '@/components/CyclingScrambleText';
 import VerletClothString from '@/components/VerletClothString';
 import Button from '@/components/Button';
 
+// Import assets properly from src/assets
+import resumePDF from '@/assets/resume.pdf';
+import linkedinIcon from '@/assets/linkedin.png';
+import githubIcon from '@/assets/github.png';
 
 function App() {
 
   return (
     <>
       <div>
-       <h1 className = "greeting"> Hi!, my name is</h1> 
-       <h1 className = "name" >Kushal Gadamsetty</h1> 
-       <h1 className = "position">I <CyclingScrambleText 
-          words={['code', 'lift','build', 'create']}
-          interval={2100} // 2.5 seconds between changes
-          scrambleConfig={{
-            speed: 0.33,
-            scramble: 6,
-            seed: 1,
-            chance: 1,
-            tick: 1,
-            range: [97, 122], // A-Z, a-z, and some symbols
-            
-          }}
-        />.</h1> 
+        <h1 className="greeting">Hi!, my name is</h1>
+        <h1 className="name">Kushal Gadamsetty</h1>
+        <h1 className="position">
+          I{' '}
+          <CyclingScrambleText 
+            words={['code', 'lift', 'build', 'create']}
+            interval={2100} // 2.5 seconds between changes
+            scrambleConfig={{
+              speed: 0.33,
+              scramble: 6,
+              seed: 1,
+              chance: 1,
+              tick: 1,
+              range: [97, 122], // a-z
+            }}
+          />
+          .
+        </h1> 
         
-      
-      <Button className="resume-button" variant="default" 
-      size="default" onClick={() => window.open('./src/assets/resume.pdf', '_blank')}>Resume</Button>
+        <Button
+          className="resume-button"
+          variant="default"
+          size="default"
+          onClick={() => window.open(resumePDF, '_blank')}
+        >
+          Resume
+        </Button>
 
+        <VerletClothString
+          position="78%"
+          iconSrc={linkedinIcon}
+          iconSize={32}
+          stringLength={350}
+          stringColor="#cadf9e"
+          stringWidth={3}
+          onClick={() => window.open('https://linkedin.com/in/kushalgadamsetty', '_blank')}
+        />
 
-
-        <VerletClothString 
-  position="78%" 
-  iconSrc="./src/assets/linkedin.png"
-  iconSize={32}
-  stringLength={350}
-  stringColor="#cadf9e"
-  stringWidth={3}
-  onClick={() => window.open('https://linkedin.com/in/kushalgadamsetty', '_blank')}
-/>
-
-        <VerletClothString 
-  position="85%" 
-  iconSrc="./src/assets/github.png"
-  iconSize={32}
-  stringLength={450}
-  stringColor="#cadf9e"
-  stringWidth={3}
-  onClick={() => window.open('https://github.com/kushallg', '_blank')}
-/>
-
-
+        <VerletClothString
+          position="85%"
+          iconSrc={githubIcon}
+          iconSize={32}
+          stringLength={450}
+          stringColor="#cadf9e"
+          stringWidth={3}
+          onClick={() => window.open('https://github.com/kushallg', '_blank')}
+        />
 
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
